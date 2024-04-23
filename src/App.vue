7ecc1aaf35a9e6b9af8e5875fc349db4e1d0ea4b
leviettho0611 <leviettho0611@gmail.com>
2024-04-23 17:54:56 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,13 @@
   <nav class="navbar navbar-light bg-light">
     <form class="container-fluid justify-content-start">
       <router-link to="/Login">
-      <button class="btn btn-outline-success me-2" type="button">Login</button>
+      <button class="btn btn-outline-secondary me-2" type="button">Login</button>
       </router-link>
       <router-link to="/Register">
-      <button class="btn btn-outline-success me-2" type="button">Register</button>
+      <button class="btn btn-outline-secondary me-2" type="button">Register</button>
       </router-link>
       <router-link to="/TodoApp">
-      <button class="btn btn-outline-success me-2" type="button">Smaller button</button>
+      <button class="btn btn-outline-secondary me-2" type="button">TodoApp</button>
     </router-link>
     </form>
   </nav>
